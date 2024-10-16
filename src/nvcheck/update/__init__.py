ae83dc08c2fe79755500623d92f52b67a7d883e5
Philipp A. <flying-sheep@web.de>
2024-10-16 17:26:06 +0200

    msg_update

--- src/nvcheck/update/__init__.py
+++ src/nvcheck/update/__init__.py
@@ -23,7 +23,7 @@ async def update_pkgbuilds(
     updated: Mapping[str, tuple[str, RichResult]], *, repo_dir: Path, pkgs_dir: Path
 ) -> None:
     updater = Updater(repo_dir, pkgs_dir)
-    async with asyncio.TaskGroup() as tg:
+    async with updater.http_client, asyncio.TaskGroup() as tg:
         for name, (oldver, new) in updated.items():
             tg.create_task(updater.update(name, oldver, new))
 
@@ -32,19 +32,22 @@ async def update_pkgbuilds(
 class Updater:
     repo_dir: Path
     pkgs_dir: Path
-    http_client: AsyncClient = field(default_factory=AsyncClient)
+    http_client: AsyncClient = field(default_factory=lambda: AsyncClient(http2=True))
 
     async def update(self, name: str, oldver: str, new: RichResult) -> None:
-        async with self.http_client:
-            match new.url:
-                case str() if (match := re.fullmatch(pypi.URL_PAT, new.url)):
-                    msg = await pypi.msg_update(
-                        self.http_client, match["name"], (oldver, match["version"])
-                    )
-                case None:
-                    msg = f"no url for {name}"
-                    raise RuntimeError(msg)
-                case _:
-                    msg = f"unknown URL pattern for {name}: {new.url}"
-                    raise RuntimeError(msg)
-            print(msg)
+        msg = await self.msg_update(name, oldver, new)
+        print(msg)
+
+    async def msg_update(self, name, oldver, new):
+        match new.url:
+            case str() if (match := re.fullmatch(pypi.URL_PAT, new.url)):
+                msg = await pypi.msg_update(
+                    self.http_client, match["name"], (oldver, match["version"])
+                )
+            case None:
+                msg = f"no url for {name}"
+                raise RuntimeError(msg)
+            case _:
+                msg = f"unknown URL pattern for {name}: {new.url}"
+                raise RuntimeError(msg)
+        return msg
