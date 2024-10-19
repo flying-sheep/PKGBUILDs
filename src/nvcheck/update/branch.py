949f1312d0bf581c22ca580d526324a93795d0e7
Philipp A. <flying-sheep@web.de>
2024-10-19 13:15:52 +0200

    all except auth

--- src/nvcheck/update/branch.py
+++ src/nvcheck/update/branch.py
@@ -1,17 +1,27 @@
 from __future__ import annotations
 
 import asyncio
+from dataclasses import dataclass, field
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import cast
+from typing import TYPE_CHECKING, cast
 
 import pygit2
 from pygit2.repository import Repository
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 
 async def create_branch(
     repo_dir: Path, pkg_dir: Path, branch: str, newver: str
 ) -> None:
+    original_repo = pygit2.Repository(str(repo_dir))
+    origin = original_repo.remotes["origin"]
+    if origin.url is None:
+        msg = f"no origin URL for {repo_dir}: {origin.url=}"
+        raise RuntimeError(msg)
+
     with TemporaryDirectory() as tmp_dir:
         repo = cast(
             Repository,
@@ -42,5 +52,27 @@ async def create_branch(
             [repo.head.target],
         )
         repo.index.write()
-        # TODO: push
-        raise AssertionError()
+
+        # “+” means force
+        repo.remotes.set_url("origin", origin.url)
+        if origin.push_url:
+            repo.remotes.set_push_url("origin", origin.push_url)
+        await push(repo.remotes["origin"], [f"+HEAD:refs/heads/{branch}"])
+
+
+@dataclass
+class RemoteCallbacks(pygit2.RemoteCallbacks):
+    future: asyncio.Future = field(default_factory=asyncio.Future)
+
+    def push_update_reference(self, refname: str, message: str | None):
+        if message is None:
+            self.future.set_result(None)
+        else:
+            msg = f"Error pushing to {refname}: {message}"
+            self.future.set_exception(RuntimeError(msg))
+
+
+async def push(remote: pygit2.Remote, specs: Iterable[str]) -> None:
+    cb = RemoteCallbacks()
+    remote.push(list(specs), callbacks=cb)
+    await cb.future
