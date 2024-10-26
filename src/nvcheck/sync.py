fc9bc9b329575bb91c2a1151a05459d69d6c3155
Philipp A. <flying-sheep@web.de>
2024-10-26 17:39:23 +0200

    fix?

--- src/nvcheck/sync.py
+++ src/nvcheck/sync.py
@@ -3,11 +3,12 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, cast
 
 import structlog
+from nvchecker.core import load_file
+
 from aurweb_client import Client
 from aurweb_client.api.package_search import get_rpc_v5_search_arg as search
 from aurweb_client.models.get_rpc_v5_search_arg_by import GetRpcV5SearchArgBy as By
 from aurweb_client.types import Unset
-from nvchecker.core import load_file
 
 from .update import COMMON_ARGS
 
