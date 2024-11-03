af3b5edefd2a30dd8fd56ff430163c082658c3d0
Philipp A. <flying-sheep@web.de>
2024-11-03 17:34:59 +0100

    More elegant read_vers

--- src/nvcheck/srcinfo.py
+++ src/nvcheck/srcinfo.py
@@ -1,20 +1,25 @@
 from __future__ import annotations
 
 from pathlib import Path
-from typing import cast
+from typing import TYPE_CHECKING, cast
 
 from srcinfo.parse import parse_srcinfo
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
 
 
 def read_vers(dir: Path | None = None) -> dict[str, str]:
     if dir is None:
         dir = Path()
-    vers: dict[str, str] = {}
+    return dict(_read_vers(dir))
+
+
+def _read_vers(dir: Path) -> Generator[tuple[str, str], None, None]:
     for d in dir.iterdir():
         if d.name.startswith(".") or not d.is_dir():
             continue
         srcinfo, errors = parse_srcinfo((d / ".SRCINFO").read_text())
         if errors:
             raise RuntimeError(f"Error parsing {d}:\n{errors}")
-        vers[d.name] = cast(str, srcinfo["pkgver"])
-    return vers
+        yield d.name, cast(str, srcinfo["pkgver"])
